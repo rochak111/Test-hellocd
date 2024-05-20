1dea6ccd352dab2ff0d9d38e22aa31b749e911c5
rochak111 <91822867+rochak111@users.noreply.github.com>
2024-05-21 00:14:40 +0545

    Update Program.cs

--- HelloCD/HelloCD/HelloCD/Program.cs
+++ HelloCD/HelloCD/HelloCD/Program.cs
@@ -13,7 +13,7 @@ namespace HelloCD
         static void Main(string[] args)
         {
             string versionNumber = majorVersion.ToString() + "." + minorVersion.ToString();
-            string message = "Hello CD! This program is version number";
+            string message = "Hello CD! test This program is version number";
             Console.WriteLine(message + " " + versionNumber);
             Console.WriteLine(message + "   test ftp again ");
             Console.WriteLine("Press any key to exit.");
