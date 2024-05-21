06905dde5c25f9758ce47aaf4a4aaa1df24cac67
rochak111 <91822867+rochak111@users.noreply.github.com>
2024-05-21 10:17:49 +0530

    Update Program.cs.....test

--- HelloCD/HelloCD/HelloCD/Program.cs
+++ HelloCD/HelloCD/HelloCD/Program.cs
@@ -15,7 +15,7 @@ namespace HelloCD
             string versionNumber = majorVersion.ToString() + "." + minorVersion.ToString();
             string message = "Hello CD! test This program is version number";
             Console.WriteLine(message + " " + versionNumber);
-            Console.WriteLine(message + "   i am in office okay tested ");
+            Console.WriteLine(message + "   i am in office----See you ------ okay tested ");
             Console.WriteLine("Press any key to exit.");
             Console.ReadLine();
         }
